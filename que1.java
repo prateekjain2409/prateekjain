d132fac5c149020e6b0ee766d9245872f4edc175
prateekjain2409 <60730917+prateekjain2409@users.noreply.github.com>
2020-02-11 11:10:31 +0530

    codingtest

---
+++ que1.java
@@ -0,0 +1,18 @@
+package codingtest;
+
+public class que1 {
+	
+	    public static void main(String args[])
+	    {
+	        int i, j;
+	        for(i=0; i<4; i++)
+	        {
+	            for(j=0; j<=i; j++)
+	            {
+	                System.out.print("*");
+	            }
+	            System.out.println();
+	        }
+	    }
+}
+	
